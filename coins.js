e672eb5afccec277a984a4b2cb577a31fcb18ff6
David Maust <david@dmaust.net>
2019-08-16 23:45:24 -0700

    initial commit

---
+++ coins.js
@@ -0,0 +1,42 @@
+
+const _ = require('lodash');
+
+/* coinValues must be in descending order 
+   each value must be an even multiple of the preceding value.
+*/
+const coinValues = {
+    quarter: 25,
+    dime: 10,
+    nickle: 5,
+    penny: 1
+};
+
+function coinGen(cents){
+    
+    // Create an empty result object
+    let results = {};
+
+    for(const coinType in coinValues){
+
+        // Find whole number of coins that divide cents
+        results[coinType] = Math.floor(cents / coinValues[coinType]);
+
+        // Assign cents to the remainder after dividing by the value
+        cents %= coinValues[coinType];
+    }
+    return results;
+}
+
+let testCents = _.range(100);
+
+for (const i in testCents){
+    const cents = testCents[i];
+    console.log("Cents:", cents);
+    console.log("Change", coinGen(cents));
+}
+
+
+// testCents.forEach(function(cents){
+//     console.log("Cents:", cents);
+//     console.log("Change", coinGen(cents));
+// });
